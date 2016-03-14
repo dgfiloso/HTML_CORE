b6547970d794cb4b6e36d65c599f92104d721d1e
dgfiloso <dgfiloso95@gmail.com>
2016-03-14 21:06:20 +0100

    Nueva modificacion de servidor.js

--- P6/servidor.js
+++ P6/servidor.js
@@ -9,40 +9,56 @@ if(process.argv.length != 3){
 
 var fichero = process.argv[2];		//Nombre del fichero con la agenda
 var port = 8000;			//Puerto en el que vamos a ejecutar la aplicacion
+var sockets = new Array();		//Array con los sockets
+var agenda = new Array();		//Array con todos los contactos
 
 			//Funcion que se ejecutara para añadir un contacto en la agenda
-var setTel = function(name,tel){
+var setTel = function(name,tel,socket){
 	fs.exists(fichero, function(exists){		//Comprobamos si existe el fichero donde vamos a guardar el contacto
 		if(exists){
-			fs.readFile(fichero, function(err,data){		//Si existe leemos el archivo para ver si ya tenemos el contacto
-				if(err){
-					throw "KO";			//Si no podemos abrir correctamente el fichero mandamos el mensaje KO
-				}else{
-					var linea = data.split("\n");		//Tomamos cada linea del fichero y la almacenamos en linea
-					for(var i=0; i<linea.length; i++){			//Recorremos cada linea
-						var palabra = linea[i].split(", ");		//Separamos el nombre del numero de telefono
-						for(var j=0; j<palabra.length; j++){
-							if(palabra[0] == name){				//Si el nombre ya esta guardado, cambiamos el telefono
-									//Cambiar el telefono
-							}else{
-								fs.appendFile(fichero, name+", "+tel+"\n", function(err){		//Si el nombre no esta guardado, lo añadimos como contacto
-									if(err){
-										throw "KO";
-									}else{
-										socket.write("OK");
-									}
-								})
-							}
+			var contacto = {nombre:name,telf:tel};
+			fs.unlink(fichero);
+			var contactoExistente = 0;
+			var error = 0;
+			agenda.forEach(function(cont){
+				if(cont.nombre === name){
+					contactoExistente = 1;
+					cont.telf = tel;
+					fs.appendFile(fichero, cont.nombre+", "+cont.telf+"\n", function(err){
+						if(err){
+							error = 1;
+							throw "KO\n";
 						}
-					}
+					});
+				}else{
+					fs.appendFile(fichero, cont.nombre+", "+cont.telf+"\n", function(err){
+						if(err){
+							error = 1;
+							throw "KO\n";
+						}
+					});
 				}
-			})		
+			});
+			if(contactoExistente === 0){
+				agenda.push(contacto);
+				fs.appendFile(fichero, contacto.nombre+", "+contacto.telf+"\n", function(err){
+					if(err){
+						error = 1;
+						throw "KO\n";
+					}
+				});
+			}
+			if(error === 1){
+				throw "KO\n";
+			}else{
+				socket.write("OK\n");
+			}
 		}else{					//Si no existe el fichero de la agenda, lo creamos y añadimos el contacto
 			fs.writeFile(fichero, name+", "+tel+"\n", function(err){
 				if(err){
-					throw "KO";
+					throw "KO\n";
 				}else{
-					socket.write("OK");
+					socket.write("OK\n");
 				}
 			})
 		}
@@ -50,52 +66,70 @@ var setTel = function(name,tel){
 }
 
 			//Funcion que se ejecuta para obtener el telefono de un contacto pasado como parametro
-var getTel = function(name){
+var getTel = function(name, socket){
 	fs.exists(fichero, function(exists){		//Comprobamos que existe el fichero de la agenda
 		if(exists){
-			fs.readFile(fichero, function(err,data){		//Si existe el fichero, lo leemos
-				if(err){
-					throw "KO";
-				}else{
-					var linea = data.split("\n");		//Separamos cada linea del fichero
-					for(var i=0; linea.length; i++){		
-						var palabra = linea[i].split(", ");		//Para cada linea, separamos el nombre del telefono
-						if(name==palabra[0]){
-							socket.write(palabra[1]);		//Si existe el contacto, devolvemos su telefono
-						}else{
-							socket.write("KO");
-						}
-					}
+			var existe = 0;
+			agenda.forEach(function(cont){
+				if(cont.nombre === name){
+					existe = 1;
+					socket.write(cont.telf+"\n");
 				}
-			})
+			});
+			if(existe === 0){
+				socket.write("KO\n");
+			}
 		}
 	})
 }
 
 			//Funcion que cierra la conexion de un socket
 var quit = function(socket){
+	socket.write("Cerrando...\n");
 	var i = sockets.indexOf(socket);
-	socket.splice(i,1);
+	sockets.splice(i,1);
 }
 
 			//Creamos el servidor
 var server = net.createServer(function(socket){
-	socket.write("Servidor creado");		//Después de crearlo, lo primero que hacemos es indicar que ha sido creado
+	socket.write("Servidor creado\n");		//Después de crearlo, lo primero que hacemos es indicar que ha sido creado
 
-	socket.push(socket)				//Abrimos el socket
+	fs.exists(fichero, function(exists){
+		if(exists){
+			fs.readFile(fichero, 'utf-8', function(err,data){
+				if(err){
+					throw "KO\n";
+				}else{
+					var linea = data.split("\n");
+					for(var i=0; i<linea.length; i++){
+						var palabra = linea[i].split(", ");
+						var contacto = {nombre:palabra[0],telf:palabra[1]};
+						agenda.push(contacto);
+					}
+				}
+			})
+		}
+	});
 
-	socket.on('data',function(err,d){			//Funcion a realizar cuando hay datos en el socket
-		var comando = data.split(" ");			//Separamos el comando por palabras
-		if(data.match(/^setTel/)=='setTel'){		//Si la primera palabra es 'setTel', ejecutamos la funcion setTel(name,tel)
-			setTel(comando[1]+" "+comando[2], comando[3]);		
-		}else if(data.match(/^getTel/)=='getTel'){		//Si la primera palabra es 'getTel', ejecutamos la funcion getTel(name)
-			getTel(comando[1]+" "+comando[2]);
-		}else if(data.match(/^quit/)=='quit'){			//Si la primera palabra es 'quit', ejecutamos la funcion quit()
+	sockets.push(socket);				//Abrimos el socket
+	socket.setEncoding("utf8");			//Establecemos que los datos van a estar en UTF-8
+
+	socket.on('data',function(data){			//Funcion a realizar cuando hay datos en el socket
+		var datos = data.trim();
+		var comando = datos.split('"');			//Separamos el comando por palabras
+
+									//Si la primera palabra es 'setTel', ejecutamos la funcion setTel(name,tel)
+		if((datos.match(/^setTel/)!==null)&&(datos.match(/^setTel/)[0]==='setTel')){		
+			setTel(comando[1], comando[2], socket);	
+									//Si la primera palabra es 'getTel', ejecutamos la funcion getTel(name)
+		}else if((datos.match(/^getTel/)!==null)&&(datos.match(/^getTel/)[0]==='getTel')){		
+			getTel(comando[1], socket);
+									//Si la primera palabra es 'quit', ejecutamos la funcion quit()
+		}else if((datos.match(/^quit/)!==null)&&(datos.match(/^quit/)[0]==='quit')){			
 			quit(socket);
 		}else{
-			throw err;
+			socket.write("KO\n");
 		}
-
 	});
 });
 
